a948c94db721c779e7549f632ed4526100a811e2
verlnn <mlnsukim2327@gmail.com>
2023-06-10 17:13:42 +0900

    Initial commit

---
+++ src/main/java/com/example/roomshceduler/RoomShcedulerApplication.java
@@ -0,0 +1,13 @@
+package com.example.roomshceduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoomShcedulerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RoomShcedulerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/roomshceduler/RoomShcedulerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.roomshceduler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoomShcedulerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
